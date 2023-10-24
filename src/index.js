92f0958eeaade394e6f6afd74c16d40aec936ff1
AlexandrAAkulina <icomfy1484@gmail.com>
2023-10-24 12:14:12 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+import Notiflix from 'notiflix';
+
+
+axios.defaults.headers.common["x-api-key"] = "live_aqGn0sP4yhYYK1NbT5HhLP2rLSG7wibrGzoTxoIhjK584UPeuEEgXmsBAa7H9QIX";
+
---
+++ src/js/cat-api.js
@@ -0,0 +1,4 @@
+export const fetchBreeds = breed => {
+    const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+    
+}
